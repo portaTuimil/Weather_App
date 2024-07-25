2b243a6c2a81215304c2ac04454d3676784b950a
Pablo Porta <pabloportatuimil12@gmail.com>
2024-07-25 10:45:45 +0200

    CSS refactored

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ async function retrieveData(city){
         let data = await response.json();
         let currentData = data.current;
         let forecast = data.forecast.forecastday[0];
-        console.log(currentData)
-        console.log(forecast.hour[2].time, forecast.hour[2].temp_c, forecast.hour[2].temp_f, forecast.hour[2].condition.text, forecast.hour[2].condition.icon);
-        //loadData(data);
+        console.log(data.forecast.forecastday[0].hour[1].time);
         buildInterface(data);
     
     } catch(error){
@@ -22,50 +20,33 @@ async function retrieveData(city){
 function buildInterface(data){
     content.firstElementChild.remove();
     content.classList.add("rowContainer");
-    const fRow = document.createElement("div");
-    const sRow = document.createElement("row");
-    const tRow = document.createElement("row");
-    content.appendChild(fRow).classList.add("row1");
-    content.appendChild(sRow).classList.add("row2");
-    content.appendChild(tRow).classList.add("row3");
 
     //Current weather Card:
     const currentCard = document.createElement("div")
-    fRow.setAttribute("id", "currentCard")
-    fRow.appendChild(currentCard).classList.add("card", "text-bg-dark");
+    const sRow = document.createElement("row");
+
     const title = document.createElement("h2");
-    let img = document.createElement('img'); 
-    img.src = "https:" + data.current.condition.icon;  
     title.textContent = (data.location.name + ", " + data.location.localtime + ":");
+
+    const img = document.createElement('img'); 
+    img.src = "https:" + data.current.condition.icon;  
+    
     
     currentCard.appendChild(title).classList.add("title")
     currentCard.appendChild(img).classList.add("currentSVG"); 
+    content.appendChild(currentCard).classList.add("card", "text-bg-dark");
+    content.appendChild(sRow).classList.add("row2");
 
     //Weather by hour:
+    for(let i = 0; i < 24; i++){
+        let wCard = document.createElement("div");
+        let hourText = document.createElement("p");
+        hourText.textContent = data.forecast.forecastday[0].hour[i].time.slice(-6);
+        wCard.classList.add("card", "text-bg-dark", "hourDiv");
+        sRow.appendChild(wCard)
+        wCard.appendChild(hourText)
+    }
 }
-
-
-
-/*
-function loadData(data){
-    title.textContent = data.current.condition.text;
-    while (imgContainer.firstChild){
-        imgContainer.firstChild.remove();
-    };
-    let img = document.createElement('img'); 
-    img.src = "https:" + data.current.condition.icon;  
-    
-    
-}*/
-
-
-
-
-
-
-
-
-
 
 //Calls:
 const submit = document.querySelector("#submit");
